ae6752b570626f98add00649fe52851710e5ab60
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2022-12-22 22:06:42 -0500

    Finished Day 01 Project

---
+++ day01/BandNameGenerator.py
@@ -0,0 +1,9 @@
+# Day 01 of 100 Days of Python
+
+print("Welcome to the Band Name Generator \n")
+
+city = input("What's the name of the city you grew up in? \n")
+
+pet = input("What's your pet's name? \n")
+
+print(f"Your band name could be {city} {pet}")
