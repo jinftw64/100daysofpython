74f96decb2f157dc5cec40d40060fadbd54166a0
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2023-01-04 22:28:24 -0500

    Complete Day 6 Escaping the Maze.

--- day06/EscapingTheMaze.py
+++ day06/EscapingTheMaze.py
@@ -4,22 +4,20 @@
 # Follow along right edge of maze
 # 1. Turn right
 # 2. Go straight if can't go right
-# 3. Turn left as last resortself.
+# 3. Turn left as last resort.
 
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
 
-def go_right():
-    turn_right()
-    move()
-
 while not at_goal():
-    while not right_is_clear():
+    if right_is_clear():
         turn_right()
-    turn_right()
-    if front_is_clear():
+        move()
+    elif front_is_clear():
         move()
     else:
-        pass
+        while not front_is_clear():
+            turn_left()
+        move()
