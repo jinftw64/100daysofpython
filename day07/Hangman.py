b0dfa47301477bb065dea92b253b90a29d1c0c27
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2023-01-11 11:34:13 -0500

    Add function to draw player lives.

--- day07/Hangman.py
+++ day07/Hangman.py
@@ -6,11 +6,70 @@ word_list = ["ardvark", "baboon", "camel"]
 chosen_word = choice(word_list)
 word_length = len(chosen_word)
 display = []
+lives = 6
+end_of_game = False
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 for letter in range(word_length):
     display.append("_")
 
-while "_" in display:
+while not end_of_game:
     print(f"This is the solution: {chosen_word}")
     guess = input("Guess a letter: ").lower()
 
@@ -19,6 +78,15 @@ while "_" in display:
         if letter == guess:
             display[position] = letter
 
-    print(display)
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            print("You lose.")
+            end_of_game = True
 
-print("You win!")
+    if "_" not in display:
+        print("You win!")
+        end_of_game = True
+
+    print(f"{' '.join(display)}")
+    print(stages[lives])
