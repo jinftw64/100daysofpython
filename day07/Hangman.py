6bafa539b75fa3c237536841616cf3fdc240af53
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2023-01-13 22:08:05 -0500

    Add print statements to recap their previous action.

--- day07/Hangman.py
+++ day07/Hangman.py
@@ -1,18 +1,16 @@
 # 100 Days of Code
 # Day 7
 from random import choice
-import hangman_art
-import hangman_words
+from hangman_art import logo, stages
+from hangman_words import word_list
 
-word_list = hangman_words.word_list
 chosen_word = choice(word_list)
+guessed_words = set()
 word_length = len(chosen_word)
 display = []
 lives = 6
 end_of_game = False
 
-stages = hangman_art.stages
-logo = hangman_art.logo
 print(logo)
 
 for letter in range(word_length):
@@ -21,7 +19,12 @@ for letter in range(word_length):
 while not end_of_game:
     print(f"{' '.join(display)}")
     print(stages[lives])
+
     guess = input("Guess a letter: ").lower()
+    if guess in guessed_words:
+        print(f"'{guess}' - You've already guessed this letter.")
+    else:
+        guessed_words.add(guess)
 
     for position in range(word_length):
         letter = chosen_word[position]
@@ -29,6 +32,7 @@ while not end_of_game:
             display[position] = letter
 
     if guess not in chosen_word:
+        print(f"'{guess}' - This letter isn't in the word")
         lives -= 1
         if lives == 0:
             print("You lose.")
